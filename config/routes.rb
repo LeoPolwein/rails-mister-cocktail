f3253abbcbb90e5b44242287d14554ffe33c137f
Leo Polwein <leo.polwein@gmx.de>
2019-08-16 17:53:16 +0200

    delete

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,9 @@ Rails.application.routes.draw do
   # get 'cocktails/index'
   # get 'cocktails/show'
   # get 'cocktails/new'
-  # get 'cocktails/create'
+  # delete 'cocktails/:id', to: 'cocktails#destroy', as: :cocktail_delete
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :cocktails, only: [:index, :show, :new, :create] do
+  resources :cocktails, only: [:index, :show, :new, :create, :destroy] do
     resources :doses, only: [:new, :create]
   end
   resources :doses, only: [:destroy]
